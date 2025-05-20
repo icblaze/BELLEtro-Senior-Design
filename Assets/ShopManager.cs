2f40b281211ae7681c91b6ea17e110489c32ca3d
Frederick B <89982743+bouloutef04@users.noreply.github.com>
2025-05-20 13:00:45 -0400

    Shop UI Scene Added (Look at description)
    Created the Shop UI and some scripts for the Shop Manager.

---
+++ Assets/ShopManager.cs
@@ -0,0 +1,14 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+using Unity.VisualScripting;
+using System.Threading.Tasks;
+//Script is used to create the functionality of the shop. This includes
+//buying jokers/mentors, buying ante vouchers, buying packs, rerolling,
+//going to the next round and calling functions for cards as necessary.
+public class ShopManager : MonoBehaviour
+{
+    private Button nextRoundButton;
+}
