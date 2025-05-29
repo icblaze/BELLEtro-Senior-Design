3e5cc9f043e9fe3b855af5b21d7a375a9e9426c8
Frederick B <89982743+bouloutef04@users.noreply.github.com>
2025-05-29 13:56:50 -0400

    HowToPlay Menu Finished
    HowToPlay menu is finished.
    
    The page buttons work and remain within the bounds of the text. The text goes over a comprehensive "guide" of the game. (If any more is needed, please let me know).

--- Assets/MainMenuManager.cs
+++ Assets/MainMenuManager.cs
@@ -118,7 +118,7 @@ public class MainMenuManager : MonoBehaviour
     //In this case, it is the next page.
     public void NextPage()
     {
-        if (text.pageToDisplay == 4)
+        if (text.pageToDisplay == 14)
         {
             //Nothing
         }
