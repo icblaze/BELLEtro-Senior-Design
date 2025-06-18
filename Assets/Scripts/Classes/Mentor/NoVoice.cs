f03fe652d4a44821f08d2b6ecdf7f6a38e9bc0a2
Andy Van <oneandyvan@gmail.com>
2025-06-18 18:29:05 -0400

    Added card-by-card override for Mentors needed for some Mentors

---
+++ Assets/Scripts/Classes/Mentor/NoVoice.cs
@@ -0,0 +1,28 @@
+﻿// This Document contains the code for the NoVoice mentor
+// Effect is to have played cards with Voiceless/Lax suit give +3 Mult when scored
+// Current Devs:
+// Andy (flakkid): Created mentor
+
+using UnityEngine;
+using System.Collections;
+
+
+public class NoVoice : Mentor
+{
+    //  Mentor name and basePrice are preset
+    public NoVoice(CardEdition edition) : base(MentorName.NoVoice, edition, 4)
+    {
+        locations = new UseLocation[] { UseLocation.PreCard };
+        description = "Played cards with Voiced/Tense suit give +3 Mult when scored";
+    }
+
+    //  Played card will add +3 Mult if Voiceless/Lax suit
+    public override void UseMentor(PCard card)
+    {
+        if(card.suit == SuitName.Voiceless)
+        {
+            Debug.Log("+3 Mult since Voiceless");
+            //  TODO add +3 to Mult variable of round
+        }
+    }
+}
---
+++ Assets/Scripts/Classes/Mentor/Valentine.cs
@@ -0,0 +1,28 @@
+﻿// This Document contains the code for the Valentine mentor
+// Effect is to have played cards with Voiced/Tense suit give +3 Mult when scored
+// Current Devs:
+// Andy (flakkid): Created mentor
+
+using UnityEngine;
+using System.Collections;
+
+
+public class Valentine : Mentor
+{
+    //  Mentor name and basePrice are preset
+    public Valentine(CardEdition edition) : base(MentorName.Valentine, edition, 4)
+    {
+        locations = new UseLocation[] { UseLocation.PreCard };
+        description = "Played cards with Voiced/Tense suit give +3 Mult when scored";
+    }
+
+    //  Played card will add +3 Mult if Voiced/Tense suit
+    public override void UseMentor(PCard card)
+    {
+        if(card.suit == SuitName.Voiced)
+        {
+            Debug.Log("+3 Mult since Voiced");
+            //  TODO add +3 to Mult variable of round
+        }
+    }
+}
