a0b3991bffb25767dde7dce65a1ce812a1523a97
Frederick B <89982743+bouloutef04@users.noreply.github.com>
2025-06-23 13:43:52 -0400

    Changes in Scene for UI, added code for TitleManager and started working on script to show current hand type.
    Added to text for the current hand in the scene for UI. Attached the objects to the script for Title Manager.
    
    Title Manager additions to setRoundTitle. Not all variables for the round are created so it will be updated in the future to have the actual names, symbols and scores appear. Added an instance so that it can be easily accessible. Removed some useless/filler code from functions.
    CurrentHandManager is used to update the current hand's text in the UI. This includes the hand name and its level. This is simply to add the script and has no meaningful code as of right now.

--- Assets/TitleManager.cs
+++ Assets/TitleManager.cs
@@ -1,4 +1,6 @@
+using TMPro;
 using UnityEngine;
+using UnityEngine.UI;
 
 //Script is used to manage the title section of the regular UI. This includes
 //when in the blind select screen, the shop, and the round screen.
@@ -6,27 +8,52 @@ using UnityEngine;
 //Fredrick Bouloute (bouloutef04)
 public class TitleManager : MonoBehaviour
 {
+    //Canvas Groups to transition
     public CanvasGroup selectBlindScreen;
     public CanvasGroup shopScreen;
     public CanvasGroup roundScreen;
+    //Text Components for Round Screne
+    public GameObject blindName;
+    public GameObject scoreRequirement;
+    public GameObject rewardAmount;
+    //Symbol/Badge for the round
+    public GameObject roundSymbol;
+    //Accessing Fade Script Functions
     FadeScript fade = FadeScript.access();
 
-    public void changeToBlindScreen()
+    private static TitleManager instance;
+    public static TitleManager access()
+    {
+        if (instance == null)
+        {
+            instance = new TitleManager();
+        }
+        return instance;
+    }
+    public void changeToRoundSelectScreen()//Change from shop to round/blind select
     {
         StartCoroutine(fade.FadeOut(shopScreen));
-        StartCoroutine(fade.FadeOut(roundScreen));
+
         StartCoroutine(fade.FadeIn(selectBlindScreen));
     }
-    public void changeToShopScreen()
+    public void changeToShopScreen()//Change from Round Screen to Shop Screen
     {
-        StartCoroutine(fade.FadeOut(selectBlindScreen));
         StartCoroutine(fade.FadeOut(roundScreen));
         StartCoroutine(fade.FadeIn(shopScreen));
     }
-    public void changeToRoundScreen()
+    public void changeToRoundScreen()//Change from Round Select to Round Screen
     {
-        StartCoroutine(fade.FadeOut(shopScreen));
+
+        setRoundTitle();
         StartCoroutine(fade.FadeOut(selectBlindScreen));
         StartCoroutine(fade.FadeIn(roundScreen));
+    }
+
+    public void setRoundTitle()//Sets the titles when loading into a round
+    {
+        //Set the round title and its components
+        scoreRequirement.GetComponent<TMP_Text>().text = "" /* + round.scoreRequirment*/;
+        rewardAmount.GetComponent<TMP_Text>().text = "$" /*+ round.reward*/;
+        roundSymbol.GetComponent<Image>().sprite = Resources.Load<Sprite>($"Round/" /*+ round.name*/);
     }
 }
