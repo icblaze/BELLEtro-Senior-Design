cb566814237ae132ce6a5257866980c5cec2a473
Frederick B <89982743+bouloutef04@users.noreply.github.com>
2025-06-29 17:17:19 -0400

    Github only pushed the meta data and not the EndOfRoundManager script.
    Idk why. It was checked and everything lol

---
+++ Assets/EndOfRoundManager.cs
@@ -0,0 +1,108 @@
+using TMPro;
+using UnityEngine;
+using UnityEngine.UI;
+//Script is used to handle the logic of the end of round screen.
+//This includes the interest and remaining hands and increasing the 
+//players moneyCount at when they cash out.
+//Current Devs:
+//Fredrick Bouloute (bouloutef04)
+public class EndOfRoundManager : MonoBehaviour
+{
+    public GameObject remainingHandsNumber;
+    public Button cashOutButton;
+    public GameObject interestNumber;
+    public GameObject roundRewardText;
+    public GameObject moneyText;
+    Game inst = Game.access();
+    Player player = Player.access();
+    int roundReward = 0;
+    int totalCashOut = 0;
+    int interest = 0;
+
+    private static EndOfRoundManager instance;  //EndOfRoundManager instance varaiable
+
+    //Singleton for the EndOfRoundManager
+    public static EndOfRoundManager access()
+    {
+        if (instance == null)
+        {
+            instance = new EndOfRoundManager();
+        }
+
+        return instance;
+    }
+
+    public void EndScreenOpened()
+    {
+        CalculateRoundReward();
+        CalculateHands();
+        CalculateInterest();
+        SetCashOut();
+    }
+    private void CalculateRoundReward()
+    {
+        int round = inst.roundValue;
+        int ante = inst.ante;
+
+        switch (round)
+        {
+            case 0:
+                roundReward = 0;
+                break;
+            case 1:
+                roundReward = 3;
+                break;
+            case 2:
+                roundReward = 4;
+                break;
+            case 3:
+                if (ante == 8)
+                {
+                    roundReward = 8;
+                    break;
+                }
+                roundReward = 5;
+                break;
+            default:
+                roundReward = 3;
+                break;
+        }
+
+        roundRewardText.GetComponent<TMP_Text>().text = "Reward: $" + roundReward.ToString();
+        totalCashOut += roundReward;
+    }
+
+    private void CalculateInterest()
+    {
+        interest = (int)player.moneyCount / 5;
+        interestNumber.GetComponent<TMP_Text>().text = interest.ToString();
+        totalCashOut += interest;
+    }
+
+    private void CalculateHands()
+    {
+        int hands = player.handCount;
+        remainingHandsNumber.GetComponent<TMP_Text>().text = hands.ToString();
+        totalCashOut += hands;
+    }
+    private void SetCashOut()
+    {
+        cashOutButton.GetComponent<TMP_Text>().text = totalCashOut.ToString();
+    }
+
+    public void CashOut()
+    {
+        //Add cash out to player's cash and change text
+        player.moneyCount += totalCashOut;
+        moneyText.GetComponent<TMP_Text>().text = "$" + player.moneyCount.ToString();
+
+        //Set everything to default/0
+        totalCashOut = 0;
+        roundReward = 0;
+        interest = 0;
+
+        //Transition to shopScreen
+        TransitionManager transitionManager = GameObject.FindGameObjectWithTag("TransitionManager").GetComponent<TransitionManager>();
+        transitionManager.TransitionToShopScreen();
+    }
+}
