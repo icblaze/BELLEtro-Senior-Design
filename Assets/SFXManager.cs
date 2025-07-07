d486dea29db3d7f89401aaf8f46b43112c3bf479
Frederick B <89982743+bouloutef04@users.noreply.github.com>
2025-07-07 13:45:53 -0500

    More changes to SFXManager and additions of SFXs.

--- Assets/SFXManager.cs
+++ Assets/SFXManager.cs
@@ -1,16 +1,65 @@
+//Script is used to ge the different soudn effects and create separate
+//functions for each sound as needed. This allows other scripts andor
+//objects to call them.
+//Current Dev: Fredrick Bouloute (bouloutef04)
 using UnityEngine;
 
 public class SFXManager : MonoBehaviour
 {
+    private static AudioManager audioManager;
+    private static AudioClip cardScoring;
+    private static AudioClip buttonPress;
+    private static AudioClip moneyUsed;
+    private static AudioClip no;//Simply for instances where something can't be done such as selected an unselectable card
+    private static AudioClip roundWon;
+    private static AudioClip gameWon;
+    private static AudioClip gameLost;
+    private static AudioClip cardSelected;
+    private static SFXManager sfxManager;
+
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        
+        audioManager = GameObject.Find("AudioManager").GetComponent<AudioManager>();
+        buttonPress = Resources.Load<AudioClip>($"SFX/Button");
+        cardScoring = Resources.Load<AudioClip>($"SFX/Hollow");
+        moneyUsed = Resources.Load<AudioClip>($"SFX/Metal Dropping");
+        roundWon = Resources.Load<AudioClip>($"SFX/Level Completion");
+        no = Resources.Load<AudioClip>($"SFX/Stingz");
+        gameWon = Resources.Load<AudioClip>($"SFX/GameWon");
+        gameLost = Resources.Load<AudioClip>($"SFX/Whoosh");
+        cardSelected = Resources.Load<AudioClip>($"SFX/CardSelected");
     }
-
-    // Update is called once per frame
-    void Update()
+    public void ButtonSFX()
     {
-        
+        audioManager.PlaySFX(buttonPress);
+    }
+    public void PurchasedMade()
+    {
+        audioManager.PlaySFX(moneyUsed);
+    }
+    public void CardScoreSFX()
+    {
+        audioManager.PlaySFX(cardScoring);
+    }
+    public void NoSFX()
+    {
+        audioManager.PlaySFX(no);
+    }
+    public void RoundWonSFX()
+    {
+        audioManager.PlaySFX(roundWon);
+    }
+    public void GameWonSFX()
+    {
+        audioManager.PlaySFX(gameWon);
+    }
+    public void GameLostSFX()
+    {
+        audioManager.PlaySFX(gameLost);
+    }
+    public void CardSelected()
+    {
+        audioManager.PlaySFX(cardSelected); 
     }
 }
