9246fd6837bcffc0866f79f4ef31b8ea192c1476
Frederick B <89982743+bouloutef04@users.noreply.github.com>
2025-07-23 18:32:42 -0400

    VolumeSliders are now persists across scenes and play throughs (aka, VolumeSlider prefs now save correctly).

--- Assets/VolumeSliders.cs
+++ Assets/VolumeSliders.cs
@@ -20,15 +20,23 @@ public class VolumeSliders : MonoBehaviour
     void Start()
     {
         if (!PlayerPrefs.HasKey("Master"))
+        {
             PlayerPrefs.SetFloat("Master", 0.7f);
+        }
 
         if (!PlayerPrefs.HasKey("Music"))
+        {
             PlayerPrefs.SetFloat("Music", -10);
+        }
 
         if (!PlayerPrefs.HasKey("SFX"))
+        {
             PlayerPrefs.SetFloat("SFX", -10);
+        }
         if (!PlayerPrefs.HasKey("Voice"))
+        {
             PlayerPrefs.SetFloat("Voice", -10);
+        }
         //Load after ensuring playerprefs exist
         Load();
     }
@@ -37,7 +45,7 @@ public class VolumeSliders : MonoBehaviour
     public void SetMasterVolume()
     {
         AudioListener.volume = MasterVolumeSlider.value;
-        Save();
+        PlayerPrefs.Save();
     }
     //Function takes in the volume slider value and sets the music parameter in mixer
     //to the value given.
@@ -45,29 +53,35 @@ public class VolumeSliders : MonoBehaviour
     {
         audioMixer.SetFloat("Music", MusicSlider.value);
         PlayerPrefs.SetFloat("Music", MusicSlider.value);
+        PlayerPrefs.Save();
     }
 
     public void SetSFXVolume()
     {
         audioMixer.SetFloat("SFX", SFXSlider.value);
         PlayerPrefs.SetFloat("SFX", SFXSlider.value);
+        PlayerPrefs.Save();
     }
     public void SetVoiceVolume()
     {
         audioMixer.SetFloat("Voice", VoiceSlider.value);
         PlayerPrefs.SetFloat("Voice", VoiceSlider.value);
+        PlayerPrefs.Save();
     }
     //Set sliders to player prefs.
     private void Load()
     {
         MasterVolumeSlider.value = PlayerPrefs.GetFloat("Master");
-        SetMasterVolume();
+        AudioListener.volume = MasterVolumeSlider.value;
+
         MusicSlider.value = PlayerPrefs.GetFloat("Music");
-        SetMusicVolume();
+        PlayerPrefs.SetFloat("Music", PlayerPrefs.GetFloat("Music"));
+  
         SFXSlider.value = PlayerPrefs.GetFloat("SFX");
-        SetSFXVolume();
+        audioMixer.SetFloat("SFX", PlayerPrefs.GetFloat("SFX"));
+
         VoiceSlider.value = PlayerPrefs.GetFloat("Voice");
-        SetVoiceVolume();
+        audioMixer.SetFloat("Voice", PlayerPrefs.GetFloat("Voice"));
     }
 
     private void Save()
@@ -76,5 +90,6 @@ public class VolumeSliders : MonoBehaviour
         PlayerPrefs.SetFloat("Music", MusicSlider.value);
         PlayerPrefs.SetFloat("SFX", SFXSlider.value);
         PlayerPrefs.SetFloat("Voice", VoiceSlider.value);
+        PlayerPrefs.Save();
     }
 }
